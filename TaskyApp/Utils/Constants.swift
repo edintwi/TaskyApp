75e6daf894e51e4116fcbcd9ffeae34adcad7c9d
Edson Brandon <edsonbrandon8@gmail.com>
2024-07-02 21:28:36 -0300

    setup to view code and adding assets

---
+++ TaskyApp/Utils/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  TaskyApp
+//
+//  Created by Edson Brandon on 02/07/24.
+//
+
+import Foundation
+
+enum AssetsConstants {
+    static let entryIlustration = "entry-ilustration"
+    static let taskyIlustration = "tasky-ilustration"
+    static let logo = "logo"
+    static let darkPurple = "darkPurple"
+    static let lilic = "lilic"
+}
